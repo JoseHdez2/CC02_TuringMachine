284fc522afcdbf62f5256f8a61b3fa1d92034a17
Jose Hernandez <josemanuelhdez2@gmail.com>
2015-10-01 11:16:26 +0100

    Errors. Created StringProcessing class.

--- CC_01_StackAutomaton/src/main/AutomataCreator.java
+++ CC_01_StackAutomaton/src/main/AutomataCreator.java
@@ -21,38 +21,80 @@ import java.util.regex.Pattern;
 public abstract class AutomataCreator {
 
 	final static Charset ENCODING = StandardCharsets.UTF_8;
-	final String EMPTY_LINE = "\\s*";
 
-	
-	Automaton createAutomatonB(String fileName) throws IOException {
-
+	/**
+	 * Reads the lines of a file and returns them as an ArrayList
+	 * @param fileName
+	 * @return An ArrayList<String> where each of the elements is a line.
+	 * @throws IOException
+	 */
+	ArrayList<String> readFileLines(String fileName) throws IOException {
 		Path path = Paths.get(fileName);
-		ArrayList<String> lines = new ArrayList<String>();
-
-		// 1) Read file lines.
+		ArrayList<String> fileLines = new ArrayList<String>();
 		
 		try (Scanner scanner = new Scanner(path, ENCODING.name())) {
 			while (scanner.hasNextLine()) {
-				lines.add(scanner.nextLine());
+				fileLines.add(scanner.nextLine());
 			}
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
 
-		ArrayList<String> emptyLines = new ArrayList<String>();
-		ArrayList<ArrayList<String>> tokenizedLines = new ArrayList<ArrayList<String>>();
-
-		// Tokenize valid lines and discard useless ones.
-		for (String line : lines) {
-			if (Pattern.matches(EMPTY_LINE, line)) {
-				emptyLines.add(line);
-				continue;
-			}
-			ArrayList<String> tokenizedLine = new ArrayList<String>(
-					Arrays.asList(line.trim().split("\\s+")));
-			tokenizedLines.add(tokenizedLine);
+		return fileLines;
+	}
+	
+	/**
+	 * For each string in stringList, only keep the left part of the leftmost '#' if there is any.
+	 * @param stringList
+	 * @return	stringList with all of its comments stripped.
+	 */
+	ArrayList<String> stripComments(ArrayList<String> stringList) {
+		
+		for (String str : stringList) {
+			str = str.split("#")[0];
 		}
-		lines.removeAll(emptyLines);
+		
+		return stringList;
+	}
+	
+	/**
+	 * Removes all empty and whitespace lines from an ArrayList<String>.
+	 * @param stringList
+	 * @return A copy of stringList, with all empty (and whitespace) lines removed.
+	 */
+	ArrayList<String> removeAllEmptyLines(ArrayList<String> stringList) {
+		final String EMPTY_LINE = "\\s*";
+
+		ArrayList<String> emptyLines = new ArrayList<String>();
+		
+		for (String str : stringList) {
+			if (Pattern.matches(EMPTY_LINE, str))
+				emptyLines.add(str);
+		}
+		stringList.removeAll(emptyLines);
+		
+		return stringList;
+	}
+	
+	ArrayList<String[]> tokenizeLines(ArrayList<String> stringList) {
+		// TODO: Return AL<AL<String>> instead of AL<String[]>?
+		ArrayList<String[]> tokenizedLines = new ArrayList<String[]>();
+		
+		for (String str : stringList) {
+			String[] tokens = str.split("\\s+");
+			tokenizedLines.add(tokens);
+		}
+		
+		return tokenizedLines;
+	}
+	
+	Automaton createAutomaton(String fileName) throws IOException {
+		
+		ArrayList<String> lines = readFileLines(fileName);
+		lines = stripComments(lines);
+		lines = removeAllEmptyLines(lines);
+
+		ArrayList<String[]> tokenizedLines = tokenizeLines(lines);
 
 		// 3) Convert tokens into valid constructor arguments.
 		
@@ -96,7 +138,7 @@ public abstract class AutomataCreator {
 				initialState, initialStackSymbol, transitionRules, acceptStates);
 	}
 	
-	Automaton createAutomaton(String fileName) throws IOException {
+	Automaton createAutomatonOld(String fileName) throws IOException {
 
 		Path path = Paths.get(fileName);
 		ArrayList<String> lines = new ArrayList<String>();
